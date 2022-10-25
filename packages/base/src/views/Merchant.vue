a765d86e39ccd8e7dbab3ac4aa34464282524ed7
lrurif <1984811813@qq.com>
2022-10-25 17:03:26 +0800

    feat: 静态资源路径修复

--- packages/base/src/views/Merchant.vue
+++ packages/base/src/views/Merchant.vue
@@ -7,7 +7,7 @@
 </template>
   
 <script lang="ts" setup>
-const url = process.env.NODE_ENV === 'development' ? 'http://localhost:9003/merchant' : '';
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:9003/merchant/' : '';
 const handleDataChange = (e: unknown) => {
     console.log(e, 'handleDataChange')
 }
