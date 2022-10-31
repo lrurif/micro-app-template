9d9fd5d0f20b419e798b377866c88d64e840ef10
lrurif <1984811813@qq.com>
2022-10-31 22:19:45 +0800

    请求函数抽取

--- packages/data-center/src/main.ts
+++ packages/data-center/src/main.ts
@@ -2,5 +2,13 @@ import "./publicPath.ts";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import microApp from "@micro-zoe/micro-app";
+import "@monorepo/share/style/normalize.css";
+import installPinia from "@monorepo/share/plugins/pinia";
 
-createApp(App).use(router).mount("#app");
+
+microApp.start();
+const vm = createApp(App);
+installPinia(vm);
+vm.use(router).mount("#app");
+
