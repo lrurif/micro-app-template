080fac631fcd04242db1dd0898a9a2e0ca72a410
lrurif <1984811813@qq.com>
2022-11-07 14:59:15 +0800

    第一次打包

--- push.sh
+++ push.sh
@@ -5,6 +5,7 @@ echo $2
 echo $commitId
 git add .
 git commit -m "$1"
+# git rev-parse HEAD（获取最新一次提交hash）
 commitId=`git rev-parse --short HEAD`
-git tag $2 commitId
-# git push
+git tag $2 $commitId
+git push
