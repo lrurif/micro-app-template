dbe41aec18a9b4e627de6e6c8451a0931c866e46
lrurif <1984811813@qq.com>
2022-11-07 18:33:24 +0800

    优化

--- packages/data-center/src/layout/empty.vue
+++ packages/data-center/src/layout/empty.vue
@@ -1,6 +1,6 @@
 <template>
-    <router-view></router-view>
+    <router-view>
+    </router-view>
 </template>
 <script lang="ts" setup></script>
-<style lang="scss">
-</style>
+<style lang="scss"></style>
--- packages/data-center/src/views/overview-page/DataOverview.vue
+++ packages/data-center/src/views/overview-page/DataOverview.vue
@@ -1,16 +1,24 @@
 <template>
-    <div>数据看板</div>
-    <el-input v-model="inputValue" />
-    <el-button @click="handleClick">发送</el-button>
+    <div>
+        <div>数据看板</div>
+        <el-input v-model="inputValue" />
+        <el-button @click="handleClick">发送</el-button>
+    </div>
 </template>
 <script lang="ts" setup>
 import { ref, inject } from "vue";
+import { useRouter } from "vue-router"
 const inputValue = ref("");
-const eventBus:any = inject("eventBus");
+const eventBus: any = inject("eventBus");
+const router = useRouter();
+
 const handleClick = () => {
+    router.push({
+        name: "userList"
+    })
     document.querySelector("input").focus();
-    console.log(eventBus, 'eventBus');
-    eventBus.emit("hide", '123', '123')
+    console.log(eventBus, "eventBus");
+    eventBus.emit("hide", "123", "123");
 };
 </script>
 <style lang="scss"></style>
