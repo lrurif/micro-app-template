8ea3ea1e7156e502fad8058d0c479789b7c7899c
lrurif <1984811813@qq.com>
2022-11-08 18:30:18 +0800

    feat: 优化

--- push.sh
+++ push.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 git pull
-echo $1
-echo $2
-echo $commitId
 git add .
 git commit -m "$1"
 # git rev-parse HEAD（获取最新一次提交hash）
 commitId=`git rev-parse --short HEAD`
-git tag -a $2 $commitId
+git tag -a $2 -m $1 $commitId
 git push origin $2
